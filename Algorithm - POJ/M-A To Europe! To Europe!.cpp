037d7f630b0e606c23ffb34cb5b6d093e81e2631
Zhilong <cyprestar@outlook.com>
2018-01-05 23:42:33 +0800

    Minor fixes

--- Algorithm - POJ/M-A To Europe! To Europe!.cpp
+++ Algorithm - POJ/M-A To Europe! To Europe!.cpp
@@ -10,29 +10,29 @@ double t[NUM];
 
 int main(void)
 {
-	int b, l, n;
-	scanf("%d %d %d", &b, &l, &n);
-	while (b != 0 && l != 0 && n != 0) {
-		l *= 60;
-		for (int i = 1; i <= n; i++) {
-			scanf("%d %d", &w[i], &s[i]);
-		}
-		t[0] = 0;
-		for (int i = 1; i <= n; i++) {
-			int slowest_speed = s[i];
-			int load = w[i];
-			t[i] = (double)l / (double)s[i] + t[i - 1];
-			for (int j = i - 1; j >= 1; j--) {
-				load += w[j];
-				if (load > b) {
-					break;
-				}
-				slowest_speed = slowest_speed < s[j] ? slowest_speed : s[j];
-				t[i] = (t[i] < (t[j - 1] + (double)l / (double)slowest_speed)) ? t[i]: (t[j - 1] + (double)l / (double)slowest_speed);
-			}
-		}
-		printf("%.1lf\n", t[n]);
-		scanf("%d %d %d", &b, &l, &n);
-	}
-	return 0;
+    int b, l, n;
+    scanf("%d %d %d", &b, &l, &n);
+    while (b != 0 && l != 0 && n != 0) {
+        l *= 60;
+        for (int i = 1; i <= n; i++) {
+            scanf("%d %d", &w[i], &s[i]);
+        }
+        t[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int slowest_speed = s[i];
+            int load = w[i];
+            t[i] = (double)l / (double)s[i] + t[i - 1];
+            for (int j = i - 1; j >= 1; j--) {
+                load += w[j];
+                if (load > b) {
+                    break;
+                }
+                slowest_speed = slowest_speed < s[j] ? slowest_speed : s[j];
+                t[i] = (t[i] < (t[j - 1] + (double)l / (double)slowest_speed)) ? t[i]: (t[j - 1] + (double)l / (double)slowest_speed);
+            }
+        }
+        printf("%.1lf\n", t[n]);
+        scanf("%d %d %d", &b, &l, &n);
+    }
+    return 0;
 }
--- Algorithm - POJ/M-E 重要逆序对.cpp
+++ Algorithm - POJ/M-E 重要逆序对.cpp
@@ -9,61 +9,61 @@ int num[10000010];
 int temp[10000010];
 
 long long mergesort(int a, int b) {
-	long long sum = 0;
-	if ((b - a) <= 0) {
-		return sum;
-	}
-	sum += mergesort(a, (a + b) / 2);
-	sum += mergesort((a + b) / 2 + 1, b);
-	int x = a;
-	int y = (a + b) / 2 + 1;
-	int mid = (a + b) / 2;
-	// 重要逆序对，要求i < j 且 ai > 2aj
-	// 所以先算一遍有多少ai > 2aj
-	while (x <= (a + b) / 2 && y <= b) {
-		if (num[x] > 2 * num[y]) {
-			sum += mid - x + 1;
-			y++;
-		}
-		else {
-			x++;
-		}
-	}
-	// 然后再做Merge Sort
-	x = a;
-	y = (a + b) / 2 + 1;
-	int i = a;
-	while (x <= (a + b) / 2 && y <= b) {
-		if (num[x] < num[y]) {
-			temp[i++] = num[x++];
-		}
-		else {
-			temp[i++] = num[y++];
-		}
-	}
-	// 同样，后面不用再算了，因为前面都已经算过了
-	while (x <= (a + b) / 2) {
-		temp[i++] = num[x++];
-	}
-	while (y <= b) {
-		temp[i++] = num[y++];
-	}
-	for (int i = a; i <= b; i++) {
-		num[i] = temp[i];
-	}
-	return sum;
+    long long sum = 0;
+    if ((b - a) <= 0) {
+        return sum;
+    }
+    sum += mergesort(a, (a + b) / 2);
+    sum += mergesort((a + b) / 2 + 1, b);
+    int x = a;
+    int y = (a + b) / 2 + 1;
+    int mid = (a + b) / 2;
+    // 重要逆序对，要求i < j 且 ai > 2aj
+    // 所以先算一遍有多少ai > 2aj
+    while (x <= (a + b) / 2 && y <= b) {
+        if (num[x] > 2 * num[y]) {
+            sum += mid - x + 1;
+            y++;
+        }
+        else {
+            x++;
+        }
+    }
+    // 然后再做Merge Sort
+    x = a;
+    y = (a + b) / 2 + 1;
+    int i = a;
+    while (x <= (a + b) / 2 && y <= b) {
+        if (num[x] < num[y]) {
+            temp[i++] = num[x++];
+        }
+        else {
+            temp[i++] = num[y++];
+        }
+    }
+    // 同样，后面不用再算了，因为前面都已经算过了
+    while (x <= (a + b) / 2) {
+        temp[i++] = num[x++];
+    }
+    while (y <= b) {
+        temp[i++] = num[y++];
+    }
+    for (int i = a; i <= b; i++) {
+        num[i] = temp[i];
+    }
+    return sum;
 }
 
 int main(void) {
-	long long res;
-	scanf("%d", &n);
-	while (n > 0) {
-		for (int i = 0; i < n; i++) {
-			scanf("%d", &num[i]);
-		}
-		res = mergesort(0, n - 1);
-		printf("%lld\n", res);
-		scanf("%d", &n);
-	}
-	return 0;
+    long long res;
+    scanf("%d", &n);
+    while (n > 0) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &num[i]);
+        }
+        res = mergesort(0, n - 1);
+        printf("%lld\n", res);
+        scanf("%d", &n);
+    }
+    return 0;
 }
